ea20094ce8e7052d7b5e2d71787a5b17d566d4ac
Matěj <matej.gazda@gmail.com>
2022-09-07 10:05:28 +0200

    Update predlohaBiometrie.js
    Var převedeno na let/const

--- predlohaBiometrie.js
+++ predlohaBiometrie.js
@@ -1,6 +1,6 @@
-var title, user, pin, pinCollection, fileName, building, userNoAccent, signer, serverRelativeUrlToFolder, DocumentTypeId;
-var appWebUrl = "/sites/WF/Biometrie"; //TODO: ZMĚNIT NA SLOŽKU
-var hostWebUrl = "/sites/WF";
+let title, user, pin, pinCollection, fileName, building, userNoAccent, signer, serverRelativeUrlToFolder, DocumentTypeId, newName;
+const appWebUrl = "/sites/WF/Biometrie"; //TODO: ZMĚNIT NA SLOŽKU
+const hostWebUrl = "/sites/WF";
 
 function sendToBiometrics() {
   title = ""; //TODO: NÁZEV POLOŽKY (IDEÁLNĚ UNIKÁTNÍ - NESMÍ TAM BÝT ZNAKY, CO NEZVLÁDNE URL)
@@ -11,7 +11,7 @@ function sendToBiometrics() {
   serverRelativeUrlToFolder = "AppKeyRecordsDoc/NewDocuments"; //TODO: ZMĚNIT NA SLOŽKU PRO APLIKACI, TAKŽE "AppName/NewDocuments"
   DocumentTypeId = 1 //TODO: typ dokumentu, default 1
 
-  var dd = getPdfData();  //TODO: Dole upravit funkci "getPdfData", která vytváří PDF
+  const dd = getPdfData();  //TODO: Dole upravit funkci "getPdfData", která vytváří PDF
   const pdfDocGenerator = pdfMake.createPdf(dd);
   pdfDocGenerator.getBuffer().then(function (value) {
     uploadFile(value);
@@ -20,7 +20,7 @@ function sendToBiometrics() {
 
 function getPdfData() {
   // playground requires you to assign document definition to a variable called dd
-  var dd = {}; //TODO: TVŮJ JSON PRO PDF
+  const dd = {}; //TODO: TVŮJ JSON PRO PDF
   return dd;
 }
 
@@ -30,25 +30,18 @@ function uploadFile(data) {
   // Define the folder path for this example.
 
   // Get test values from the file input and text input page controls.
-  var fileInput = jQuery("#getFile");
-  var newName = fileName;
+  newName = fileName;
 
-  // Get the server URL.
-  var serverUrl = _spPageContextInfo.webAbsoluteUrl;
-
-  // Initiate method calls using jQuery promises.
-  // Get the local file as an array buffer.
-  var getFile = data;
   // Add the file to the SharePoint folder.
-  var addFile = addFileToFolder(data);
+  const addFile = addFileToFolder(data);
   addFile.done(function (file, status, xhr) {
     // Get the list item that corresponds to the uploaded file.
-    var getItem = getListItem(file.d.ListItemAllFields.__deferred.uri);
+    const getItem = getListItem(file.d.ListItemAllFields.__deferred.uri);
     getItem.done(function (listItem, status, xhr) {
       // Change the display name and title of the list item.
-      var changeItem = updateListItem(listItem.d.__metadata);
+      const changeItem = updateListItem(listItem.d.__metadata);
       changeItem.done(function (data, status, xhr) {
-        alert("file uploaded and updated");
+        setTimeout(function() { alert("File sent to biometrics."); }, 1);
       });
       changeItem.fail(onError);
     });
@@ -57,25 +50,25 @@ function uploadFile(data) {
   addFile.fail(onError);
 
   // Get the local file as an array buffer.
-  function getFileBuffer() {
-    var deferred = jQuery.Deferred();
-    var reader = new FileReader();
-    reader.onloadend = function (e) {
-      deferred.resolve(e.target.result);
-    };
-    reader.onerror = function (e) {
-      deferred.reject(e.target.error);
-    };
-    reader.readAsArrayBuffer(fileInput[0].files[0]);
-    return deferred.promise();
-  }
+  // function getFileBuffer() {
+  //   var deferred = jQuery.Deferred();
+  //   var reader = new FileReader();
+  //   reader.onloadend = function (e) {
+  //     deferred.resolve(e.target.result);
+  //   };
+  //   reader.onerror = function (e) {
+  //     deferred.reject(e.target.error);
+  //   };
+  //   reader.readAsArrayBuffer(fileInput[0].files[0]);
+  //   return deferred.promise();
+  // }
 
   // Add the file to the file collection in the Shared Documents folder.
   function addFileToFolder(arrayBuffer) {
     // Get the file name from the file input control on the page.
 
     // Construct the endpoint.
-    var fileCollectionEndpoint = String.format(
+    const fileCollectionEndpoint = String.format(
       "{0}/_api/web/getfolderbyserverrelativeurl('{1}')/files" +
         "/add(overwrite=true, url='{2}')",
       appWebUrl,
@@ -101,9 +94,7 @@ function uploadFile(data) {
   // Get the list item that corresponds to the file by calling the file's ListItemAllFields property.
   function getListItem(fileListItemUri) {
     // Send the request and return the response.
-    console.log(fileListItemUri);
-
-    var listItemAllFieldsEndpoint = String.format(
+    const listItemAllFieldsEndpoint = String.format(
       fileListItemUri + "?@target='{1}'",
       appWebUrl,
       hostWebUrl
@@ -118,13 +109,13 @@ function uploadFile(data) {
 
   // Change the display name and title of the list item.
   function updateListItem(itemMetadata) {
-    var listItemEndpoint = itemMetadata.uri;
+    const listItemEndpoint = itemMetadata.uri;
 
     // Define the list item changes. Use the FileLeafRef property to change the display name.
     // For simplicity, also use the name as the title.
     // The example gets the list item type from the item's metadata, but you can also get it from the
     // ListItemEntityTypeFullName property of the list.
-    var body = String.format(
+    const body = String.format(
       "{{'__metadata':{{'type':'{0}'}},'FileLeafRef':'{1}','Title':'{2}','Signer':'{3}','StatusName':'New','PINCollection':'{4}','DocumentTypeId':'{5}','TakeOver':'{6}'}}",
       itemMetadata.type,
       newName,
